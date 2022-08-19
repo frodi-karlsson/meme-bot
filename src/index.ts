6b9e92862c2956100820e617bab01b0b620fccf1
frodikarlsson <fr8125ka-s@student.lu.se>
2022-08-19 10:37:51 +0200

    Add web code lol

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,14 @@ import DiscordHandler from "./discordhandler";
 import { config } from "dotenv";
 config();
 const discordHandler = new DiscordHandler(process.env.DISCORD_SECRET!); // Create a new DiscordHandler instance that starts the bot
+const express = require('express')
+const app = express()
+const port = process.env.PORT ?? 3000
+
+app.get('/', (req: any, res: any) => {
+  res.send('Hello World!')
+})
+​
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
