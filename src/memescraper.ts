580df25d0fc5c2bc1b16431cb64145449bfc6c94
frodikarlsson <fr8125ka-s@student.lu.se>
2022-08-19 10:07:58 +0200

    check if response is correct

--- src/memescraper.ts
+++ src/memescraper.ts
@@ -1,9 +1,29 @@
 import fetch from 'node-fetch';
+
+interface MemeResponse {
+    postlink: string;
+    subreddit: string;
+    title: string;
+    url: string;
+    nsfw: string;
+    spoiler: boolean;
+    author: string;
+    ups: number;
+    preview: string[];
+}
+
 export default class MemeScraper {
     apiURL = 'https://meme-api.herokuapp.com/gimme';
+    private isMemeResponse(response: any): response is MemeResponse {
+        return response.postlink !== undefined && response.subreddit !== undefined && response.title !== undefined && response.url !== undefined && response.nsfw !== undefined && response.spoiler !== undefined && response.author !== undefined && response.ups !== undefined && response.preview !== undefined;
+    }
     async run(): Promise<string> {
         const response = await fetch(this.apiURL);
         const json = await response.json();
-        return json.url as string;
+        if(json && this.isMemeResponse(json)) {
+            return json.url as string;
+        } else {
+            return "";
+        }
     }
 }
