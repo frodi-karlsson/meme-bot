7cdb632ac220583fce6299f2a3599d953a3097f9
frodikarlsson <fr8125ka-s@student.lu.se>
2022-08-21 16:29:27 +0200

    Add reaction voting

--- src/discordhandler.ts
+++ src/discordhandler.ts
@@ -1,4 +1,4 @@
-import { Client, Message } from 'discord.js';
+import { Client, Message, MessageReaction, User } from 'discord.js';
 import MemeScraper from './memescraper';
 
 export default class DiscordHandler {
@@ -33,23 +33,32 @@ export default class DiscordHandler {
             } else if(message.content.toLowerCase() === "!memehelp"){
                 message.channel.send("!meme - sends a meme\n!memehelp - this help message\nReplying to a meme with +2 or -2 will rate it up or down\nReplying to a meme with cringe deletes it");
             }
-            if(meme != "") message.channel.send(meme + "\nRating: " + (this.ratingMap.get(message.id) ?? "0"));
             
+            if(meme != "") {
+                const msg: Message = await message.channel.send(meme + "\nRating: " + (this.ratingMap.get(message.id) ?? "0"));
+                msg.react('leturmemesbedreams');
+                msg.react('chugjugmoment');
+            } 
+        }).on('messageReactionAdd', async (reaction, user) => {
+            if(!reaction.partial && !user.partial) this.handleReaction(reaction, user);
         });
+    }
+
+    private async handleReaction(reaction: MessageReaction, user: User) {
+        if(reaction.message.channel.id !== "1010075098176307282" || user.id === this.client.user!.id || reaction.message.author !== this.client.user) return;
+        if(reaction.emoji.name === 'leturmemesbedreams' || reaction.emoji.name === 'chugjugmoment') {
+            this.rate(reaction.message.id, reaction.emoji.name === 'chugjugmoment');
+            reaction.message.edit(reaction.message.content?.split("\n").slice(0, -1).join("\n") + "\nRating: " + (this.ratingMap.get(reaction.message.id) ?? "0"));
+            reaction.remove();
+        }
+        
     }
 
     private async handleReply(message: Message) {
         const repliedID = message.reference?.messageId;
         if(repliedID) {
             const repliedMessage = await message.channel.messages.fetch(repliedID);
-            if (message.content.toLowerCase() === 'cringe' && repliedID && repliedMessage && repliedMessage.content.includes('Rating: ') && repliedMessage.author.id === this.client.user?.id) {
-                repliedMessage.delete();
-            } else if (message.content === '+2' || message.content === '-2' && repliedMessage && repliedMessage.author.id === this.client.user?.id && repliedMessage.content.slice(0, 4) === 'http') {
-                const positive = message.content === '+2';
-                this.rate(repliedID, positive);
-                message.delete();
-                repliedMessage.edit(repliedMessage.content.split("\n").slice(0, -1).join("\n") + "\nRating: " + (this.ratingMap.get(repliedID) ?? "0"));
-            } else if(repliedMessage.author.id === message.author.id && message.content === "!meme") {
+            if(repliedMessage.author.id === message.author.id && message.content === "!meme") {
                 this.sendMeme(repliedMessage, repliedMessage.content.split(" "));
             }
         }
