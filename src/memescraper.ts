a509b04ca31dfca85bd3619bde8e57498226bade
frodikarlsson <fr8125ka-s@student.lu.se>
2022-08-22 14:54:55 +0200

    reset maybe fixes idk

--- src/memescraper.ts
+++ src/memescraper.ts
@@ -14,11 +14,29 @@ interface MemeResponse {
 
 export default class MemeScraper {
     apiURL = 'https://meme-api.herokuapp.com/gimme';
+    subreddits = [
+        "dankmemes",
+        "memeeconomy",
+        "surrealmemes",
+        "deepfriedmemes",
+        "4chan",
+        "okbuddyretard",
+        "memes",
+        "terriblefacebookmemes",
+        "antimeme",
+        "wholesomememes",
+    ]
+    private getRandomSubreddit(): string {
+        return this.subreddits[Math.floor(Math.random() * this.subreddits.length)];
+    }
+    private getRandomSubredditLink(): string {
+        return this.apiURL + '/' + this.getRandomSubreddit();
+    }
     private isMemeResponse(response: any): response is MemeResponse {
         return response.url !== undefined && response.subreddit !== undefined && response.title !== undefined;
     }
     async run(): Promise<string> {
-        const response = await axios.get(this.apiURL);
+        const response = await axios.get(this.getRandomSubredditLink());
         const json = await response.data;
         if(json && this.isMemeResponse(json)) {
             return json.url + '\nTitle: ' + json.title + '\nSubreddit: ' + json.subreddit;
